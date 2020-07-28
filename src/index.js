462d40eef8f8d73fa8afb1a3ac53c17fa919d4a4
Dalay Salchak <okeanol777@gmail.com>
2020-07-28 14:24:17 +0700

    prod test

--- src/index.js
+++ src/index.js
@@ -37,17 +37,17 @@ iAx.post('auth/session', {})
 store.subscribe(() => {
     iAx.defaults.headers.common["X-CSRF-Token"] = store.getState().authReducer.auth.csrf;
     iAx.defaults.headers.common["Authorization"] = "Bearer " + store.getState().authReducer.auth.token;
+    ReactDOM.render(
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <Router>
+                    <App/>
+                </Router>
+            </PersistGate>
+        </Provider>,
+        document.getElementById('root')
+    );
 });
-ReactDOM.render(
-    <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-            <Router>
-                <App/>
-            </Router>
-        </PersistGate>
-    </Provider>,
-    document.getElementById('root')
-);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
